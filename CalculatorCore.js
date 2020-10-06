04ee58c90d1711db7fcd1a06c156c737ecc65835
KathrinShpigel <kathrin.shpigel.90@gmail.com>
2020-10-06 18:00:33 +0300

    feat: add view calculator

---
+++ CalculatorCore.js
@@ -0,0 +1,55 @@
+'use strict';
+
+const operands = ['+', '-', '*', '/'];
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+
+class CalculatorCore {
+  view = '0';
+
+  num1 = '';
+  num2 = '';
+
+  operand = '';
+
+  in(val = '') {
+
+    if (numbers.indexOf(val) !== -1) {
+      if (this.operand) {
+        this.num2 += val;
+      } else {
+        this.num1 += val;
+      }
+    } else if (operands.indexOf(val) !== -1) {
+      if (this.operand) {
+        this.compute();
+      }
+
+      this.operand = val;
+    } else if (val === '.') {
+      if (this.operand && this.num2.indexOf('.') === -1) {
+        this.num2 += val;
+      } else if (!this.operand && this.num1.indexOf('.') === -1) {
+        this.num1 += val;
+      }
+    } else if (val === '=' && this.num2) {
+      this.compute();
+    }
+
+    this.view = this.num1 + this.operand + this.num2;
+  }
+
+  compute() {
+    this.num1 = String(eval(this.num1 + this.operand + this.num2));
+    this.num2 = '';
+    this.operand = '';
+  }
+
+  clear() {
+    this.view = '0';
+
+    this.num1 = '';
+    this.num2 = '';
+
+    this.operand = '';
+  }
+}
--- index.js
+++ index.js
@@ -1,55 +1,19 @@
-const operands = ['+', '-', '*', '/'];
-const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+'use strict';
 
-class CalculatorCore {
-  view = '0';
+const buttons = document.querySelectorAll('[data-button]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
 
-  num1 = '';
-  num2 = '';
+const calculator = new CalculatorCore();
 
-  operand = '';
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    calculator.in(button.innerHTML);
+    currentOperandTextElement.innerHTML = calculator.view;
+  })
+});
 
-  /*
-    num1
-    operand - num1
-    num2 - operand
-    = - num2
-    +- - num1
-    view - 0 || (num1 + operand) || num2
-   */
-  in(val) {
-    // validation
-
-    if (numbers.indexOf(val) !== -1) {
-      if (this.operand) {
-        this.num2 += val;
-      } else {
-        this.num1 += val;
-      }
-    } else if (operands.indexOf(val) !== -1) {
-      if (this.operand) {
-        this.compute();
-      }
-
-      this.operand = val;
-    } else if (val === '.') {
-      if (this.operand && this.num2.indexOf('.') === -1) {
-        this.num2 += val;
-      } else if (!this.operand && this.num1.indexOf('.') === -1) {
-        this.num1 += val;
-      }
-    } else if (val === '=' && this.num2) {
-      this.compute();
-    }
-
-    this.view = this.num1 + this.operand + this.num2;
-  }
-
-  compute() {
-    this.num1 = String(eval(this.num1 + this.operand + this.num2));
-    this.num2 = '';
-    this.operand = '';
-  }
-}
-
-module.exports = CalculatorCore;
+allClearButton.addEventListener("click", () => {
+  currentOperandTextElement.innerHTML = '';
+  calculator.clear();
+});
